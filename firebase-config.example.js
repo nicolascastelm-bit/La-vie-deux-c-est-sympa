9609cd4b380c9d604bbe72522119b88a1b236176
nicolascastelm-bit <nicolas.castelm@gmail.com>
2026-07-30 21:40:36 +0200

    Delete firebase-config.example.js

--- firebase-config.example.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "TON_PROJECT_ID.firebaseapp.com",
-  projectId: "TON_PROJECT_ID",
-  storageBucket: "TON_PROJECT_ID.firebasestorage.app",
-  messagingSenderId: "TON_MESSAGING_SENDER_ID",
-  appId: "TON_APP_ID"
-};
