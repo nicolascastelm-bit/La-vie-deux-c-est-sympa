7589fd254f94ab46073953d9b9fe48c4d298588c
nicolascastelm-bit <nicolas.castelm@gmail.com>
2026-07-31 21:50:51 +0200

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyDtTcczi-30_hdn31djAr0vcF3Ztbl8U24",
-  authDomain: "plaisir-malin-et-coquin.firebaseapp.com",
-  projectId: "plaisir-malin-et-coquin",
-  storageBucket: "plaisir-malin-et-coquin.firebasestorage.app",
-  messagingSenderId: "1071445418608",
-  appId: "1:1071445418608:web:3276d6ea7b6a7d17308ab5",
-  measurementId: "G-L6CQ8KNLT6"
-};
